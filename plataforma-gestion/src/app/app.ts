8f7bd4bac865613da0dde9266f7be49125734f3f
Toriewe <claugb.99@gmail.com>
2026-09-22 02:46:25 -0500

    creacion de control flow

--- plataforma-gestion/src/app/app.ts
+++ plataforma-gestion/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { Interpolacion } from './components/interpolacion/interpolacion';
 import { Binding } from './components/binding/binding';
+import { PgControlFlow } from './components/pg-control-flow/pg-control-flow';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, Interpolacion, Binding],
+  imports: [Header, Interpolacion, Binding, PgControlFlow],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
