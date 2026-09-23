72e97f830ca9e321505c934d45e5181541eb0f48
Toriewe <claugb.99@gmail.com>
2026-09-22 20:47:22 -0500

    formulario reactivo

--- plataforma-gestion/src/app/components/pages/registro/registro.ts
+++ plataforma-gestion/src/app/components/pages/registro/registro.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
 import { Header } from '../../header/header';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Solicitud } from '../../../servicios/solicitud';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [Header, FormsModule],
+  imports: [Header, ReactiveFormsModule],
   templateUrl: './registro.html',
   styleUrl: './registro.scss',
 })
 
 export class Registro {
-  nombre = '';
-  apellido = '';
-  estado = false;
-  celular = '';
+  formulario = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    apellido: new FormControl('', Validators.required),
+    celular: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]{9}')
+    ]),
+    estado: new FormControl(false)
+  });
 
   constructor(
     private solicitud: Solicitud,
@@ -26,10 +31,10 @@ export class Registro {
   registrar() {
 
     const nuevoDato = {
-      nombre: this.nombre,
-      apellido: this.apellido,
-      celular: this.celular,
-      estado: this.estado
+      nombre: this.formulario.value.nombre!,
+      apellido: this.formulario.value.apellido!,
+      estado: this.formulario.value.estado!,
+      celular: this.formulario.value.celular!
     };
 
     this.solicitud.guardarDatos(nuevoDato).subscribe({
